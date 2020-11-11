35e856e4eadf25409e5c924809f806128f6a6df7
Antony RAMON <github@mybluemails.com>
2020-11-11 14:46:34 +0100

    Models OK

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,4 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
   validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :description, presence: true
 end
