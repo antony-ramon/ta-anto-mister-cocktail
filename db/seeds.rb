deaf4410939923d1b52a02c0fdea560c3acb2b8f
Antony RAMON <github@mybluemails.com>
2020-11-11 14:19:39 +0100

    Prepare rails app with external specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+puts "Destroy ingredients"
+Ingredient.destroy_all if Rails.env.development?
+
+puts "Destroy Cocktails"
+Cocktail.destroy_all if Rails.env.development?
+
+puts "Create ingredients, cocktails, and doses"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  ingredient_name = ingredient["strIngredient1"]
+  cocktail_name = "Cocktail with #{ingredient_name}!"
+  i = Ingredient.new(name: ingredient_name)
+  i.save
+  c = Cocktail.new(name: cocktail_name)
+  i.save
+  d = Dose.new(description: "Dose for #{c.name} / #{i.name} !", cocktail: c, ingredient: i)
+  puts "create #{i.name}, #{c.name}, #{d.description}"
+end
